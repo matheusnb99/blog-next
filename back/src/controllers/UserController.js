052ffb1cabf2232016a9ba9e6022d0febdfd2342
matheusnb99 <matheusintelectual123@hotmail.com>
2022-05-12 00:04:45 +0200

    modified permitions on route /users/:id

--- back/src/controllers/UserController.js
+++ back/src/controllers/UserController.js
@@ -2,33 +2,30 @@ import UserModel from "../db/models/UserModel.js"
 import hashPassword from "../hashPassword.js"
 
 export const users_get_list = async (req, res) => {
-  const users = await UserModel.query().withGraphFetched("[posts, comments, role]")
-    .where("deleted_at", null)
+  const users = await UserModel.query().withGraphFetched("[posts, comments, role]").where("deleted_at", null)
   res.send(users)
 }
 
 export const users_get_description = async (req, res) => {
   const {
     params: { userId },
-    session: { userId: sessionUserId },
+    //session: { userId: sessionUserId },
   } = req
 
+  /*
   if (Number(userId) !== sessionUserId) {
-		// eslint-disable-next-line no-console
-    console.log(sessionUserId)
     res.status(403).send({ error: "forbidden" })
 
     return
   }
+  */
 
-  const user = await UserModel.query().findById(userId)
-    .where("deleted_at", null)
+  const user = await UserModel.query().findById(userId).where("deleted_at", null)
 
   if (!user) {
     res.status(404).send({ error: "not found" })
 
-    
-return
+    return
   }
 
   res.send(user)
